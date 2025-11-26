8b9636819618a86e318b25071c0c9b7ac6bc75c6
creatixlabHQ <9198951281shubham@gmail.com>
2025-11-27 01:20:34 +0530

    Update submit.js
    Ok

--- api/submit.js
+++ api/submit.js
@@ -5,19 +5,22 @@ export default async function handler(req, res) {
     return res.status(405).json({ error: "Only POST allowed" });
   }
 
+  console.log("REQ BODY →", req.body);
+
   const { name, email } = req.body;
 
   try {
     const transporter = nodemailer.createTransport({
       service: "gmail",
       auth: {
-        user: process.env.MY_MAIL,   // set in Vercel env
-        pass: process.env.MY_PASS    // set in Vercel env (app password)
+        user: process.env.9198951281shubham@gmail.com,
+        pass: process.env.fryy sjdz jvev qfre
       }
     });
 
     await transporter.sendMail({
-      from: email,
+      from: process.env.MY_MAIL,
+      replyTo: email,
       to: process.env.MY_MAIL,
       subject: "New Form Submission",
       text: `Name: ${name}\nEmail: ${email}`
@@ -25,6 +28,7 @@ export default async function handler(req, res) {
 
     return res.status(200).json({ message: "Email sent!" });
   } catch (err) {
+    console.error("EMAIL ERROR →", err);
     return res.status(500).json({ error: err.message });
   }
 }
